790247394785d865b9b19bc0a6814d16d225152a
Ilona <ilona.brinkmeier@gmx.net>
2023-11-05 19:24:04 +0100

    Feat: add utils.py, cgt font

---
+++ Egypt_Pharaoh_Hieroglyphs/src/utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S python3 -i
+
+"""
+Delivers the projects root path.
+
+Author: Ilona Brinkmeier
+Date: Nov. 2023
+"""
+
+##########################
+# imports
+##########################
+
+from pathlib import Path
+
+##############################
+# coding
+##############################
+
+def get_project_root() -> Path:
+    ''' Returns path to project "Egypt_Pharaoh_Hieroglyphs" '''
+    return Path(__file__).parent.parent
